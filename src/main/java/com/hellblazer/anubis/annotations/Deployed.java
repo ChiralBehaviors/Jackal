90602be4545eb76956388da8e6c13cc8c472707b
Hal Hildebrand <hal.hildebrand@gmail.com>
2011-01-07 12:55:48 +0000

    copyright info

--- src/main/java/com/hellblazer/anubis/annotations/Deployed.java
+++ src/main/java/com/hellblazer/anubis/annotations/Deployed.java
@@ -1,3 +1,19 @@
+/** (C) Copyright 2010 Hal Hildebrand, All Rights Reserved
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.hellblazer.anubis.annotations;
 
 import java.lang.annotation.Documented;
@@ -13,7 +29,7 @@ import java.lang.annotation.Target;
  * provide any post construction configuration that must occur before active
  * systems start up.
  * 
- * <a href="mailto:hal.hildebrand@gmail.com">Hal Hildebrand</a>
+ * @author <a href="mailto:hal.hildebrand@gmail.com">Hal Hildebrand</a>
  * 
  */
 @Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE })
