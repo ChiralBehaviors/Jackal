3188d98b9b3bf5eeea9c762e7ba9f3d8abbe33c9
Hal Hildebrand <hal.hildebrand@gmail.com>
2011-01-17 01:32:14 +0000

    remove pos

--- waiter/src/main/java/com/softwarecraftsmen/dns/messaging/serializer/Serializable.java
+++
@@ -1,8 +0,0 @@
-/**
- * This file is Copyright © 2008 Software Craftsmen Limited. All Rights Reserved.
- */
-package com.softwarecraftsmen.dns.messaging.serializer;
-
-public interface Serializable {
-    void serialize(final AtomicWriter writer);
-}
